aa5ad5345f29ebeda2b864fb5be321ff110e9ecd
Prabhakaran <prabhakaran.chinnathambi@gmail.com>
2021-12-11 21:24:09 -0500

    Created my first react app

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function showTime() {
+  const element = (
+    <>
+    <h1>Current date is {new Date().toLocaleDateString()}</h1>
+    <h1> Current time is {new Date().toLocaleTimeString()}</h1>
+    </>
+  );
+  reactDom.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+setInterval(showTime, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
